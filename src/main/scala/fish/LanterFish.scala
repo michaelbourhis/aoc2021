46c3b21514999d68908d203b9319762007b2932a
Michaël Bourhis <michael.bourhis@dataiku.com>
2021-12-09 10:56:54 +0100

    day 6

---
+++ src/main/scala/fish/LanterFish.scala
@@ -0,0 +1,33 @@
+package fish
+
+import scala.annotation.tailrec
+
+object LanterFish {
+  val cycleLength = 6
+  val cycleInitLength = 2
+
+  type FishState = Map[Int, Long]
+
+  def initCycle(representation: String): FishState =
+    representation
+      .split(",")
+      .map(_.toInt)
+      .groupBy(identity)
+      .view.mapValues(_.length.toLong)
+      .toMap
+
+  @tailrec
+  def reproduction(fishState: FishState, daysLeft: Int): FishState =
+    if (daysLeft == 0)
+      fishState
+    else {
+      val reproducingFish = fishState.get(0).map(numberOfFish =>
+        Seq((cycleLength, numberOfFish), (cycleLength + cycleInitLength, numberOfFish))
+      ).getOrElse(Seq.empty).toMap
+      val otherFish = fishState.keys.filterNot(_ == 0).map(day => (day - 1, fishState(day)))
+      val newFishState = reproducingFish ++ otherFish.map{
+        case (day,numberOfFish) => day -> (numberOfFish + reproducingFish.getOrElse(day,0L))
+      }
+      reproduction(newFishState, daysLeft - 1)
+    }
+}
---
+++ src/main/scala/puzzles/day6/Part1.scala
@@ -0,0 +1,25 @@
+package puzzles.day6
+
+import fish.LanterFish
+
+import scala.io.Source
+import scala.util.Using
+
+object Part1 extends App {
+  val numberOfDays = 80
+
+  def numberOfFishAtTheEnd: Long =
+    Using.resource(Source.fromResource("day6.txt")) {
+      file =>
+        val initialFishState = file
+          .getLines()
+          .map(LanterFish.initCycle)
+          .next()
+        LanterFish
+          .reproduction(initialFishState, numberOfDays)
+          .values
+          .sum
+    }
+
+  Console.println(f"There will be $numberOfFishAtTheEnd fish after $numberOfDays days.")
+}
